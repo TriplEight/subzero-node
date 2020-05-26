96fdcae584313c2877049184097d7c4ef4a5602b
2075 <marco@one.io>
2020-05-26 09:47:27 +0200

    feat(pallet): rename template to core

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -47,7 +47,7 @@ pub use frame_support::{
 pub use contracts::Schedule as ContractsSchedule;
 
 /// Importing a template pallet
-pub use template;
+pub use subzero_core;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -101,8 +101,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("subzero-node"),
+	impl_name: create_runtime_str!("subzero-node"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
@@ -282,7 +282,7 @@ impl sudo::Trait for Runtime {
 }
 
 /// Used for the module template in `./template.rs`
-impl template::Trait for Runtime {
+impl subzero_core::Trait for Runtime {
 	type Event = Event;
 }
 
@@ -301,7 +301,7 @@ construct_runtime!(
 		TransactionPayment: transaction_payment::{Module, Storage},
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		SubzeroCore: subzero_core::{Module, Call, Storage, Event<T>},
 		//
 		Contracts: contracts::{Module, Call, Config, Storage, Event<T>},
 	}
